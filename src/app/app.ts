e001534480f723d5899175b23e636b32e2889360
ShreyaaK <shreyakdev@gmail.com>
2026-06-08 09:48:17 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from "../shared/components/sidebar/sidebar";
+import { Home } from "../features/home/home";
+import { About } from "../features/about/about";
+import { Skills } from "../features/skills/skills";
+import { Projects } from "../features/projects/projects";
+import { Contact } from "../features/contact/contact";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar, Home, About, Skills, Projects, Contact],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/features/contact/contact.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-contact',
+    standalone: true,
+    template: ` <h2>Contact Us</h2>
+                <h2>  Shreya Karanjkar  </h2>
+                <h4>shreyaakdev@gmail.com</h4>
+                <h4>https://www.linkedin.com/feed/</h4>
+                <h4>https://github.com/settings/profile</h4>
+                <h4>8261890019</h4>`,
+    styles: ''
+})
+export class Contact{}
